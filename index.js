0a08dd1e06081656f890ac9489295db3331ada1f
George <me@georgebutter.com>
2018-12-29 13:05:26 +1100

    :sparkles: Login

--- index.js
+++ index.js
@@ -5,17 +5,48 @@ var app = express();
 // Authentication
 var passport = require('passport');
 var localStrategy = require('passport-local').Strategy;
+var session = require('express-session');
+var MongoStore = require('connect-mongo')(session);
 
 // Rendering
 var expressLiquid = require('express-liquid');
 
+// Data
+var mongoose = require('mongoose');
+var Staff = require('./models/staff').Staff;
+
+mongoose.connect('mongodb://localhost/testForAuth');
+var db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', function () {
+  console.log('Connected to database');
+});
+
+// Middleware
+var bodyParser = require('body-parser');
+
 // General
 var port = 49152;
+
+// Sessions for tracking logins
+app.use(session({
+  secret: 'open up',
+  resave: true,
+  saveUninitialized: false,
+  store: new MongoStore({
+    mongooseConnection: db
+  })
+}));
+
 
 // Global templating
 var site = {
   name: 'George Butter',
 }
+
+// parse incoming requests
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
 
 // Setup liquid rendering
 var options = {
@@ -38,17 +69,40 @@ app.get('/', (req, res) => {
 });
 
 app.get('/admin', (req, res, next) => {
-  console.log(req.parsed);
-  res.render('admin-login', {
-    site: site,
-    page_title: 'Admin',
-    canonical_url: `${req.protocol}${req.protocol ? '://' : '' }${req.hostname}${req.path}`,
-  });
+  Staff.findById(req.session.userId)
+    .exec((error, user) => {
+      if (error) {
+        return next(error);
+      } else {
+        if (user === null) {
+          res.render('admin-login', {
+            site: site,
+            page_title: 'Admin',
+            canonical_url: `${req.protocol}${req.protocol ? '://' : '' }${req.hostname}${req.path}`,
+            template: 'admin-login'
+          });
+        } else {
+          res.render('admin-dashboard', {
+            site: site,
+            page_title: 'Dashboard',
+            canonical_url: `${req.protocol}${req.protocol ? '://' : '' }${req.hostname}${req.path}`,
+          });
+        }
+      }
+    })
 });
 
 app.post('/admin', (req, res, next) => {
-  console.log('admin login attempt')
-  res.redirect('/admin');
+  Staff.authenticate(req.body.username, req.body.password, (error, user) => {
+    if (error || !user) {
+      var err = new Error('Wrong username or password.');
+      err.status = 401;
+      return next(err);
+    } else {
+      req.session.userId = user._id;
+      return res.redirect('/admin');
+    }
+  });
 })
 
 app.get('/install', (req, res, next) => {
@@ -56,13 +110,73 @@ app.get('/install', (req, res, next) => {
     site: site,
     page_title: 'Install',
     canonical_url: `${req.protocol}${req.protocol ? '://' : '' }${req.hostname}${req.path}`,
+    errors: null
   });
 })
 
 app.post('/install', (req, res, next) => {
-  console.log('attempting to create admin account')
-  res.redirect('/install');
-})
+  // res.redirect('/admin');
+  const { username, email, password, confirm } = req.body;
+  const errors = [];
+  if (!username) {
+    errors.push('Username cannot be blank');
+  }
+  if (!email) {
+    errors.push('Email cannot be blank');
+  }
+  if (!password) {
+    errors.push('Password cannot be blank');
+  } else {
+    if (password.length < 6) {
+      errors.push('Password must be at least 6 characters');
+    }
+  }
+  if (password !== confirm) {
+    errors.push('Passwords don\'t match');
+  }
+
+  if (errors.length) {
+    return res.render('admin-install', {
+      site: site,
+      page_title: 'Install',
+      canonical_url: `${req.protocol}${req.protocol ? '://' : '' }${req.hostname}${req.path}`,
+      errors: errors
+    });
+  } else {
+    const userData = {
+      email: email,
+      password: password,
+      username: username,
+      confirm: confirm
+    }
+    Staff.create(userData, function (error, user) {
+      if (error) {
+        return res.render('admin-install', {
+          site: site,
+          page_title: 'Install',
+          canonical_url: `${req.protocol}${req.protocol ? '://' : '' }${req.hostname}${req.path}`,
+          errors: [error]
+        });
+      } else {
+        req.session.userId = user._id;
+        return res.redirect('/admin');
+      }
+    });
+  }
+});
+
+app.get('/admin/logout', (req, res, next) => {
+  if (req.session) {
+    // Delete session object
+    req.session.destroy((err) => {
+      if (err) {
+        return next(err);
+      } else {
+        return res.redirect('/');
+      }
+    });
+  }
+});
 
 // Listen on port
 app.listen(port, () => console.log(`George Butter site is live! 🚀`));
--- models/staff.js
+++ models/staff.js
@@ -1,5 +1,8 @@
 // Data
 var mongoose = require('mongoose');
+// Encryption
+var bcrypt = require('bcrypt');
+
 // Data Models
 var StaffSchema = new mongoose.Schema({
   email: {
@@ -23,4 +26,43 @@ var StaffSchema = new mongoose.Schema({
     required: true,
   }
 });
+
+// Authenticate input against database
+StaffSchema.statics.authenticate = function (username, password, callback) {
+  console.log(username, password)
+  Staff.findOne({ username: username })
+    .exec(function (err, user) {
+
+      if (err) {
+        return callback(err)
+      } else if (!user) {
+        var err = new Error('Staff not found.');
+        err.status = 401;
+        return callback(err);
+      }
+
+      bcrypt.compare(password, user.password, function (err, result) {
+        if (result === true) {
+          return callback(null, user);
+        } else {
+          return callback();
+        }
+      })
+    });
+}
+
+
+// Hashing a password before saving it to the database
+StaffSchema.pre('save', function (next) {
+  var user = this;
+  bcrypt.hash(user.password, 10, function (err, hash) {
+    if (err) {
+      return next(err);
+    }
+    user.password = hash;
+    next();
+  })
+});
+
 var Staff = mongoose.model('Staff', StaffSchema);
+module.exports.Staff = Staff;
