902559b3e5cf7de0b9418d4f46c54dfa420da34b
George <me@georgebutter.com>
2019-06-06 22:08:25 +1000

    :sparkles: add and commit to git

--- controllers/admin-api.js
+++ controllers/admin-api.js
@@ -6,32 +6,59 @@ const colors = require('colors');
 const repoDir = './client/theme';
 
 exports.putThemeFileJson = (req, res, next) => {
-  console.log(`[status] putThemeFileJson`.grey)
   const { key, password, content } = req.body;
   const { theme, dir, file } = req.params;
   const repo = req.app.get('repo');
-
   App.authenticate(key, password, (error, app) => {
-    console.log(error)
-    console.log(app)
     if (error || !app) {
       return res.json({
         status: 'error: Could not establish a connection'
       });
     }
-    console.log(`[status] Adding file to ${dir}`.grey)
+    if (app.themes !== 'readwrite') {
+      return res.json({
+        status: 'error: This app does not have permission to read themes'
+      });
+    }
     if (dir !== 'layouts' && dir !== 'snippets' && dir !== 'templates' && dir !== 'assets') {
       return res.json({
         status: 'error: Invalid file directory'
       })
     }
     const filePath = path.join(repo.workdir(), `${dir}/${file}`);
+    let oid;
     fs.writeFile(filePath, content, err => {
       if (err) {
         return res.json({
           status: 'error: Could not save file'
         })
       }
+      const gitIndex = app.get('gitIndex')
+      gitIndex.addAll(['.'])
+      .then(() => {
+        return gitIndex.write();
+      })
+      .then(() => {
+        return gitIndex.writeTree();
+      })
+      .then(oidResult => {
+        oid = oidResult;
+        return git.Reference.nameToId(repo, 'HEAD');
+      })
+      .then(head => {
+        return repo.getCommit(head);
+      })
+      .then(parent => {
+        const author = git.Signature.now('George Butter',
+          'threeninenineone@gmail.com');
+        const committer = git.Signature.now('George Butter',
+          'butsandcats@github.com');
+
+        return repo.createCommit('HEAD', author, committer, 'API update', oid, [parent]);
+      })
+      .done(commitId => {
+        console.log(`[git] New Commit: ${commitId}`.blue);
+      })
       return res.json({
         status: 'success'
       })
